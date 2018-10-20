a71feb44a17176956ef73dbbe7b9810d7deb2ad9
J.J. Lu <jonathan.j.lu@gmail.com>
2018-10-20 14:30:13 -0400

    python classes

---
+++ testscripts/team.py
@@ -0,0 +1,10 @@
+class nflTeam:
+    def __init__(self,city,mascot,lat,long,):
+        self.city = city
+        self.mascot = mascot
+        self.latitude = lat
+        self.longitude = long
+
+    def __repr__(self):
+        return ("The {} {} are located at {} N {} W".format(self.city,self.mascot,self.latitude,self.longitude))
+
---
+++ testscripts/teammaker.py
@@ -0,0 +1,5 @@
+import team
+
+#mascot = team.nflTeam("City","Mascot",lat,long)
+Cardinals = team.nflTeam("Arizona","Cardinals",123,456)
+
