a2ed46445e022444c4d58c1d00f4826aae0a0b22
J.J. Lu <jonathan.j.lu@gmail.com>
2018-10-25 12:39:19 -0400

    updated dataset and class file

--- scripts/NFLTeam.py
+++ scripts/NFLTeam.py
@@ -1,9 +1,10 @@
-class nflTeam:
+class NFLTeam:
     def __init__(self,city,mascot,lat,long,):
         self.city = city
         self.mascot = mascot
         self.latitude = lat
         self.longitude = long
+        self.fullName = "{},{}".format(city,mascot)
 
     def __repr__(self):
         return ("The {} {} are located at {} N {} W".format(self.city,self.mascot,self.latitude,self.longitude))
